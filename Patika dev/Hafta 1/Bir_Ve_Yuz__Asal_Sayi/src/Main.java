572db8c110e395e0de32dd09a3fa8a425216a9be
Tuba Rana Sakar <132791093+tubaranasakar@users.noreply.github.com>
2023-05-12 04:16:52 +0300

    Add files via upload

---
+++ Patika dev/Hafta 1/Bir_Ve_Yuz__Asal_Sayi/src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i = 2; i <=100; i++) {
+            boolean asal=true;
+            for (int j = 2; j < i; j++) {
+               if (i%j==0){
+                   asal=false;
+                   break;
+               }
+
+            }
+            if(asal){
+                System.out.print(i+" ");
+            }
+        }
+
+    }
+}
